3d8628a309d9e069ac94b89a0d33e41924aeb770
User2003r <itarun78072@gmail.com>
2025-11-08 11:56:23 +0530

    Add user and video models

---
+++ models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// this is predefined hook that excecutes before storing data into database
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = models?.User || model("User", userSchema);
+export default User;
